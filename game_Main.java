dc65cd9a1a966ca9bd4aeb19114c4149e1515517
Angel Quach <122626373+AngelQuach@users.noreply.github.com>
2023-08-28 23:16:54 -0400

    Update game_Main.java
    Added a new class named sarah_win to add variety to the program. The new class is intended to act as a conversation panel between the user and the fictional lead, Sarah.

--- game_Main.java
+++ game_Main.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class game_Main extends JFrame implements ActionListener{
 	//global var.
 	//Info from user
-	String name;
+	String name = "user name";
 	//layers of page
 	JLayeredPane layeredPane;
 	//bot bg, init avatar,
@@ -20,23 +20,18 @@ public class game_Main extends JFrame implements ActionListener{
 	Font txt_f = new Font("Comic Sans MS", Font.BOLD, 15);
 	
 	
-	//set up main page
+	//set up main page - constructor
 	public game_Main(starting_pg init_pg) {
-	//for testing
-	//public game_Main() {
-	//get info from user - not started yet
-		
-		
-	//layer set-up
+		//layer set-up
 		layeredPane = new JLayeredPane();
 		
-	//bg set-up (layer 1)
+		//bg set-up (layer 1)
 		table = new ImageIcon("P_bg_table.jpg");
 		table_bg = new JLabel(table);
 		layeredPane.add(table_bg, JLayeredPane.DEFAULT_LAYER);
 	
-	//panels set-up (layer 2)
-	//top panel and bot panel
+		//panels set-up (layer 2)
+		//top panel and bot panel
 		top_panel = new JPanel();
 		top_panel.setBounds(0, 0, 650, 100);
 		top_panel.setBackground(Color.DARK_GRAY);
@@ -47,9 +42,10 @@ public class game_Main extends JFrame implements ActionListener{
 		bot_panel.setBackground(Color.DARK_GRAY);
 		layeredPane.add(bot_panel, JLayeredPane.PALETTE_LAYER);
 
-	//contents set-up (layer 3)
-	//part 1: user info
-	//2023.5.30 - IDEA: user avatar personification (to be implemented)
+	   //contents set-up (layer 3)
+	   //part 1: user info --> user name & avatar
+	   //2023.5.30 - IDEA: user avatar personification (to be implemented)
+	   //2023.8.28 - UPDATE: user will be able to choose between 6 avatars --> to be designed
 		init_avar = new ImageIcon("P_init_icon.jpg");
 		init_avar_bg = new JLabel(init_avar);
 		init_avar_bg.setBounds(15, 15, 50, 50);
@@ -59,21 +55,25 @@ public class game_Main extends JFrame implements ActionListener{
 		user_name.setFont(txt_f);
 		user_name.setBounds(75, 30, 75, 20);
 		
-	//set window
+		//set window
 		this.setTitle("House Renting Simulator");
 		this.setLayeredPane(layeredPane);
 		this.setSize(650, 650);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setVisible(true);
+		
+		//regardless of the choice of the user, first show up pop-up windows to welcome them (indicator == 0 [init])
+		sarah_win welcome_win1 = new sarah_win("welcome", 0); //1st sentence said by sarah
+		//now it is time for users to name themselves (indicator == 5)
+		name = JOptionPane.showInputDialog("Hi Sarah! Thank you for the instruction! Nice to meet you, I am: ");
+	   //-------- user name collected --------
+	
+	   //system messages will demonstrate how to change avatar, followed by introductions of the panel --> to be implemented
 	}
 	
 	//mouse interaction
-		public void actionPerformed(ActionEvent e) {
-			//user's action
-			Object choice = e.getSource();
-		}
-	
-	public static void main(String args[]) {
-		new game_Main();
+	public void actionPerformed(ActionEvent e) {
+		//user's action
+		Object choice = e.getSource();
 	}
 }
